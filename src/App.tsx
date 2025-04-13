94d177e279c6441d3c94fd7d133dffb2247638f4
Heba Syijari <83651269+Heba-Syijari@users.noreply.github.com>
2025-04-13 12:48:14 +0300

    Works well on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,33 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
+} from "./components/ui/resizable"
+import { useMediaQuery } from "./hooks/use-media-query"
+import { Drawer, DrawerContent } from "./components/ui/drawer"
 
-// Create a client
 const queryClient = new QueryClient()
 
 function App() {
   const [isChatVisible, setIsChatVisible] = useState(true)
   const [searchQuery, setSearchQuery] = useState("")
+  const isMobile = useMediaQuery("(max-width: 768px)")
+  const [mobileChatOpen, setMobileChatOpen] = useState(false)
+
+  const handleChatToggle = () => {
+    if (isMobile) {
+      setMobileChatOpen(!mobileChatOpen)
+    } else {
+      setIsChatVisible(!isChatVisible)
+    }
+  }
+
+  const handleCloseChat = () => {
+    if (isMobile) {
+      setMobileChatOpen(false)
+    } else {
+      setIsChatVisible(false)
+    }
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -29,38 +48,59 @@ function App() {
               <Header
                 isChatVisible={isChatVisible}
                 searchQuery={searchQuery}
-                onChatToggle={() => setIsChatVisible(!isChatVisible)}
+                onChatToggle={handleChatToggle}
                 onSearchChange={(value) => setSearchQuery(value)}
               />
 
-              <ResizablePanelGroup direction="horizontal" className="flex-1">
-                <ResizablePanel
-                  defaultSize={70}
-                  minSize={30}
-                  className="min-w-[300px]"
-                >
-                  <TicketList
-                    onChatToggle={() => setIsChatVisible(!isChatVisible)}
-                    isChatVisible={isChatVisible}
-                  />
-                </ResizablePanel>
+              {/* Desktop Layout */}
+              {!isMobile && (
+                <ResizablePanelGroup direction="horizontal" className="flex-1">
+                  <ResizablePanel
+                    defaultSize={70}
+                    minSize={30}
+                    className="min-w-[300px]"
+                  >
+                    <TicketList
+                      onChatToggle={handleChatToggle}
+                      isChatVisible={isChatVisible}
+                    />
+                  </ResizablePanel>
 
-                {isChatVisible && (
-                  <>
-                    <ResizableHandle withHandle className="mx-1" />
-                    <ResizablePanel
-                      defaultSize={30}
-                      minSize={20}
-                      maxSize={50}
-                      className="relative"
-                    >
-                      <div className="w-full h-full relative border-l border-border">
-                        <ChatPanel onClose={() => setIsChatVisible(false)} />
-                      </div>
-                    </ResizablePanel>
-                  </>
-                )}
-              </ResizablePanelGroup>
+                  {isChatVisible && (
+                    <>
+                      <ResizableHandle withHandle className="mx-1" />
+                      <ResizablePanel
+                        defaultSize={30}
+                        minSize={20}
+                        maxSize={50}
+                        className="relative"
+                      >
+                        <div className="w-full h-full relative border-l border-border">
+                          <ChatPanel onClose={handleCloseChat} />
+                        </div>
+                      </ResizablePanel>
+                    </>
+                  )}
+                </ResizablePanelGroup>
+              )}
+
+              {/* Mobile Layout */}
+              {isMobile && (
+                <div className="flex-1 overflow-hidden">
+                  <TicketList
+                    onChatToggle={handleChatToggle}
+                    isChatVisible={mobileChatOpen}
+                  />
+                  <Drawer
+                    open={mobileChatOpen}
+                    onOpenChange={setMobileChatOpen}
+                  >
+                    <DrawerContent className="h-[80%]">
+                      <ChatPanel onClose={handleCloseChat} />
+                    </DrawerContent>
+                  </Drawer>
+                </div>
+              )}
             </div>
           </div>
         </TicketProvider>
